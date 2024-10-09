de45988173d0ab71f8876f3855599e31740371ae
Kheenan Walkins <17203992+xerotolerant@users.noreply.github.com>
2024-10-09 09:11:27 -0400

    Set default width to 100

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const container = document.querySelector('#container');
 const setupButton = document.querySelector('#setup');
 setupButton.addEventListener('click', setupGrid)
-async function setupGrid() {
+
+setupGrid(100);
+async function setupGrid(defaultWidth) {
     container.replaceChildren();
-    const userResponse = await prompt('Enter the number of pixels for grid and height');
+    let userResponse;
+    if (!defaultWidth) {
+        userResponse = await prompt('Enter the number of pixels for grid and height');
+    } else {
+        userResponse = defaultWidth;
+    }
 
     const val = parseInt(userResponse);
     const width = Math.min(val, 100);
@@ -20,17 +27,13 @@ async function setupGrid() {
             target.style.opacity = 0.1;
         } else if (target.className.includes('tile')
             && target.className.includes('hover')) {
+
             const opacity = window.getComputedStyle(target).getPropertyValue('opacity');
             const opacityVal = parseFloat(opacity);
             if (opacityVal < 1) {
                 target.style.opacity = opacityVal + 0.1;
             }
-
-
         }
-
-
-
     });
 
     for (let y = 0; y < height; y++) {
@@ -38,13 +41,12 @@ async function setupGrid() {
         row.classList.add('row');
 
         for (let x = 0; x < width; x++) {
+
             const tile = document.createElement('div');
-
             tile.classList.add('tile');
-            // tile.textContent = `${x}, ${y}`;
-
             row.appendChild(tile);
         }
+
         container.appendChild(row);
     }
 }
